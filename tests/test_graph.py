78e6486b484a67b4bf738a726a39f8e91385e25c
svedant1207 <vedantsoni1207@gmail.com>
2026-05-21 17:03:56 +0530

    dfs stk + test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,5 +1,27 @@
 import pytest
 from graph import Graph, GraphMatrix
 
+def test_bfs():
+    g = Graph()
+    for i in range(4):
+        g.add_vertex(i)
 
-def test_graph():
+    g.add_edge(0,1)
+    g.add_edge(0,2)
+    g.add_edge(1,3)
+    result = g.bfs(0)
+
+    assert result == [0,1,2,3]
+
+def test_dfs():
+    g = Graph()
+    for i in range(4):
+        g.add_vertex(i)
+
+    g.add_edge(0,1)
+    g.add_edge(0,2)
+    g.add_edge(1,3)
+
+    result = g.dfs(0)
+
+    assert result == [0,2,1,3]
